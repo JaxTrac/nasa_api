e6301302c6718f4af6d6e33ae18fa6afab5aa3eb
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-07-28 19:49:21 +0200

    Enable API call

--- code.rb
+++ code.rb
@@ -1,1 +1,9 @@
-api_key = ENV['NASA_API_KEY']
+# frozen_string_literal: true
+
+require 'nasa_apod'
+
+real_api_key = ENV['NASA_API_KEY']
+
+client = NasaApod::Client.new(api_key: real_api_key)
+result = client.search(date: '2015-06-18')
+p result
